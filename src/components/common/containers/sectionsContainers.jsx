a77faeb33f22fd6a84e25ed27dc58d7a0bb6d2fa
BetaKrankri <edominguezj11@outlook.com>
2023-04-29 17:02:28 -0600

    some layout

--- src/components/common/containers/sectionsContainers.jsx
+++ src/components/common/containers/sectionsContainers.jsx
@@ -1,32 +1,32 @@
 // contenedor con titulo superior
 export const TitledSection = ({ children, title }) => {
-    return (
-      <section
-        className="TitledSection"
+  return (
+    <section
+      className="TitledSection"
+      style={{
+        margin: "1rem 0 0",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        gap: ".875rem",
+      }}
+    >
+      <h2>{title}</h2>
+      <div
+        className="Card"
         style={{
-          margin: "1rem 0 0",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          gap: ".875rem",
+          width: "100%",
+          minHeight: 10,
+          backgroundColor: "hsl(0,0%,15%)",
+          padding: ".875rem",
+          borderRadius: "5px",
         }}
       >
-        <h2>{title}</h2>
-        <div
-          className="Card"
-          style={{
-            width: "100%",
-            minHeight: 10,
-            backgroundColor: "hsl(0,0%,15%)",
-            padding: ".875rem",
-            borderRadius: "5px",
-          }}
-        >
-          {children}
-        </div>
-      </section>
-    );
-  };
+        {children}
+      </div>
+    </section>
+  );
+};
 
 // contenedor con titulo inferior pequeño
 export const SubtitledBox = ({ children, title }) => {
@@ -42,12 +42,15 @@ export const SubtitledBox = ({ children, title }) => {
     >
       <div
         style={{
+          flex: 1,
+          display: "grid",
+          placeItems: "center",
           margin: "3px",
           border: "solid 1px hsl(0,0%,50%)",
           borderRadius: "5px",
           padding: ".25rem .25rem",
           color: "white",
-          fontSize: ".875rem",
+          fontSize: ".75rem",
           textAlign: "center",
           width: "100%",
         }}
